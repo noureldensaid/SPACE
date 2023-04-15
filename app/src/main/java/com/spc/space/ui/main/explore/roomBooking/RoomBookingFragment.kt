e1e8d2b9bcdb5e86ece15581251f7be9b724bc12
Momen <96392530+momenAboalmaged@users.noreply.github.com>
2023-04-15 07:59:08 +0200

    set_up_check_in&out_time picker

--- app/src/main/java/com/spc/space/ui/main/explore/roomBooking/RoomBookingFragment.kt
+++ app/src/main/java/com/spc/space/ui/main/explore/roomBooking/RoomBookingFragment.kt
@@ -2,11 +2,16 @@ package com.spc.space.ui.main.explore.roomBooking
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.format.DateFormat.is24HourFormat
 import android.view.View
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.spc.space.R
 import com.spc.space.databinding.FragmentRoomBookingBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -17,6 +22,70 @@ class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentRoomBookingBinding.bind(view)
         setupCalender()
+        //check in time picker
+        setuptimePicker(binding.checkInEt)
+
+        //check out time picker
+        setuptimePicker(binding.checkOutEt)
+//        binding.checkOutEt.setOnClickListener {
+//            openTimePicker { chosenTime ->
+//                binding.checkOutEt.placeholderText = chosenTime
+//                binding.checkOutEt.hint = ""
+//            }
+//        }
+
+    }
+
+    private fun setuptimePicker(editText: EditText) {
+        editText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                openTimePicker { chosenTime ->
+                    editText.hint = chosenTime
+                }
+            }
+        }
+        editText.setOnClickListener {
+            openTimePicker { chosenTime ->
+                editText.hint = chosenTime
+            }
+        }
+    }
+
+    private fun openTimePicker(callback: (String) -> Unit) {
+        val isSystem24Hour = is24HourFormat(requireContext())
+        val clockFormat =
+            if (isSystem24Hour)
+                TimeFormat.CLOCK_12H
+            else
+                TimeFormat.CLOCK_12H
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(clockFormat)
+            .setHour(10)
+            .setMinute(0)
+            .setTitleText("Choose Time")
+            .build()
+        timePicker.show(childFragmentManager, "Time Picker")
+        var chosenTime = ""
+        // listener when user choose time(click ok)
+        timePicker.addOnPositiveButtonClickListener {
+            val hour = timePicker.hour
+            val min = timePicker.minute
+            // val choosenTime="$hour : $min"
+            //  chosenTime = String.format(Locale.getDefault(), "%02d:%02d", hour, min)
+
+            val amPm = if (hour < 12) "AM" else "PM"
+
+            val formattedHour =
+                if (hour == 0)
+                    "12"
+                else if (hour <= 12)
+                    hour.toString()
+                else
+                    (hour - 12).toString()
+
+            chosenTime = String.format(Locale.getDefault(), "%s:%02d %s", formattedHour, min, amPm)
+            callback(chosenTime) // Invoke the callback with the chosen time
+        }
     }
 
     @SuppressLint("UseCompatLoadingForDrawables", "NewApi")
@@ -33,4 +102,6 @@ class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
         super.onDestroy()
         _binding = null
     }
+
+
 }
