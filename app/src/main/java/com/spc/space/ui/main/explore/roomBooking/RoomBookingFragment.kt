6070572719507982e9a5686804c2be95338dce54
nourr <nourmorgan19@gmail.com>
2023-05-11 23:58:37 +0300

    morgan

--- app/src/main/java/com/spc/space/ui/main/explore/roomBooking/RoomBookingFragment.kt
+++ app/src/main/java/com/spc/space/ui/main/explore/roomBooking/RoomBookingFragment.kt
@@ -1,21 +1,27 @@
 package com.spc.space.ui.main.explore.roomBooking
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.text.format.DateFormat.is24HourFormat
+import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import com.spc.space.R
 import com.spc.space.databinding.FragmentRoomBookingBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
+import java.time.LocalDate
 import java.util.*
 
+@RequiresApi(Build.VERSION_CODES.O)
 @AndroidEntryPoint
 class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
     private var _binding: FragmentRoomBookingBinding? = null
@@ -23,25 +29,77 @@ class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentRoomBookingBinding.bind(view)
+
         setupCalender()
         //check in time picker
         setupTimePicker(binding.checkInEt)
         //check out time picker
         setupTimePicker(binding.checkOutEt)
 
+        val time1 = binding.checkInEt.text.toString()
+        val time2 = binding.checkOut.text.toString()
+        val datte = getSelectedDay()
+
+
+        //TODO EL TIME BYB2A FADY LEEH 7TA BA3D MA E5TAR ??
 
         binding.btnConfirmBookings.setOnClickListener {
-             val checkInTime = binding.checkInEt.text.toString()
-            val checkOutTime = binding.checkOutEt.text.toString()
+            Log.e("time1", time1.toString())
+            Log.e("time2", time2.toString())
+            Log.e("date", datte)
+            if (time1.isNotBlank() && time2.isNotBlank()) {
+                val checkInTime = parseTime(binding.checkOutEt.text.toString())
+                val checkOutTime = parseTime(binding.checkInEt.text.toString())
+                val date = getSelectedDay()
+                val startTime = date + 'T' + checkInTime
+                val endTime = date + 'T' + checkOutTime
+                val roomId = ""
+                val userToken = ""
+                val isTimeCorrect = isTimeAfter(time1, time2)
 
-            if (isTimeAfter(checkInTime, checkOutTime)) {
-                findNavController().navigate(R.id.action_roomBookingFragment_to_successBookingFragment)
-            } else {
-                binding.tvCheckTimeErr.visibility=View.VISIBLE
+
+                if (isTimeCorrect) {
+                    findNavController().navigate(R.id.action_roomBookingFragment_to_successBookingFragment)
+                } else {
+                    binding.tvCheckTimeErr.visibility = View.VISIBLE
+                    Log.e("startTime", startTime.toString())
+                    Log.e("endTime", startTime.toString())
+
+                }
+
             }
+
         }
 
     }
+
+    private fun parseTime(time: String): String {
+        val inputFormat = SimpleDateFormat("h:mm a", Locale.US)
+        val outputFormat = SimpleDateFormat("HH:mm:ssZ", Locale.US)
+        val date = inputFormat.parse(time)
+        return outputFormat.format(date!!)
+    }
+
+
+    private fun getSelectedDay(): String {
+        val currentDate = LocalDate.now()
+        val y = currentDate.year
+        val m = currentDate.monthValue
+        val d = currentDate.dayOfMonth
+        val today = String.format("%04d-%02d-%02d", y, m, d)
+        var selectedDate = today
+        binding.calendarView.setOnDayClickListener(object : OnDayClickListener {
+            override fun onDayClick(eventDay: EventDay) {
+                val calendar = eventDay.calendar
+                val year = calendar.get(Calendar.YEAR)
+                val month = calendar.get(Calendar.MONTH) + 1
+                val day = calendar.get(Calendar.DAY_OF_MONTH)
+                selectedDate = String.format("%04d-%02d-%02d", year, month, day)
+            }
+        })
+        return selectedDate
+    }
+
 
     private fun setupTimePicker(editText: EditText) {
         editText.setOnFocusChangeListener { _, hasFocus ->
@@ -87,7 +145,6 @@ class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
         }
     }
 
-
     // Function to compare two time strings in the format "hh:mm AM/PM"
     private fun isTimeAfter(time1: String, time2: String): Boolean {
         val format = SimpleDateFormat("hh:mm a", Locale.getDefault())
@@ -96,26 +153,38 @@ class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
         return parsedTime1?.before(parsedTime2) ?: false
     }
 
-
     @SuppressLint("UseCompatLoadingForDrawables")
     private fun setupCalender() {
-        val calendar = Calendar.getInstance()
-        val yesterday = Calendar.getInstance()
-        yesterday.add(Calendar.DATE, -1)
-        yesterday.set(Calendar.HOUR_OF_DAY, 0)
-        yesterday.set(Calendar.MINUTE, 0)
-        yesterday.set(Calendar.SECOND, 0)
-        yesterday.set(Calendar.MILLISECOND, 0)
-
-        binding.calendarView.setMinimumDate(yesterday)
-        binding.calendarView.setDisabledDays(getDisabledDays(yesterday))
+        val calendar = binding.calendarView
         val forwardIcon = resources.getDrawable(R.drawable.ic_calendar_arrow_forward)
         val previousIcon = resources.getDrawable(R.drawable.ic_calendar_arrow_previous)
-        binding.calendarView.apply {
+        removePreviousDays()
+        calendar.apply {
             setForwardButtonImage(forwardIcon)
             setPreviousButtonImage(previousIcon)
         }
+
+//        {
+//            "room":"6457a2897a4e51c466691169",
+//            "startTime":"2023-05-07T15:00:00+02:00",
+//            "endTime":"2023-05-07T16:30:00+02:00"
+//        }
+
+
     }
+
+
+    private fun removePreviousDays() {
+        val previousDays = Calendar.getInstance()
+        previousDays.add(Calendar.DATE, -1)
+        previousDays.set(Calendar.HOUR_OF_DAY, 0)
+        previousDays.set(Calendar.MINUTE, 0)
+        previousDays.set(Calendar.SECOND, 0)
+        previousDays.set(Calendar.MILLISECOND, 0)
+        binding.calendarView.setMinimumDate(previousDays)
+        binding.calendarView.setDisabledDays(getDisabledDays(previousDays))
+    }
+
 
     private fun getDisabledDays(calendar: Calendar): List<Calendar> {
         val disabledDays = ArrayList<Calendar>()
@@ -124,14 +193,6 @@ class RoomBookingFragment : Fragment(R.layout.fragment_room_booking) {
         today.set(Calendar.MINUTE, 0)
         today.set(Calendar.SECOND, 0)
         today.set(Calendar.MILLISECOND, 0)
-
-//        val yesterday = Calendar.getInstance()
-//        yesterday.add(Calendar.DATE, -1)
-//        yesterday.set(Calendar.HOUR_OF_DAY, 0)
-//        yesterday.set(Calendar.MINUTE, 0)
-//        yesterday.set(Calendar.SECOND, 0)
-//        yesterday.set(Calendar.MILLISECOND, 0)
-
         while (calendar.before(today)) {
             disabledDays.add(calendar.clone() as Calendar)
             calendar.add(Calendar.DATE, 1)
